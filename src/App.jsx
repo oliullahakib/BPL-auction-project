438b7cd804d3108d2b0281a6c0db0ffe7f4173a5
oliullahakib <oliullahakib@gmail.com>
2025-09-22 14:43:19 +0600

    section toggoling feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
 import Banner from './components/Banner/Banner'
 import Navbar from './components/Navbar/Navbar'
 import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
 
-const playersPromiss =fetch('/public/players.json').then(res =>res.json())
+const playersPromiss = fetch('/public/players.json').then(res => res.json())
 
 function App() {
- 
+  const [isActive, setIsActive] = useState(true)
 
   return (
     <>
-     <Navbar/>
-     <Banner/>
-     <div className='section-info flex justify-between container mx-auto mt-20 mb-10'>
-      <h2 className='text-3xl font-bold'>Available Players</h2>
-      <div className='btn-container flex'>
-        <button className='p-2 border bg-[#E7FE29] text-black border-gray-300 border-r-0 rounded-l-xl'>Avaliable</button>
-        <button className='p-2 border border-gray-300 border-l-0 rounded-r-xl'>Selected(0)</button>
+      <Navbar />
+      <Banner />
+      <div className='section-info flex justify-between container mx-auto mt-20 mb-10 '>
+        <h2 className='text-3xl font-bold'>Available Players</h2>
+        <div className='btn-container flex cursor-pointer'>
+          <button onClick={()=>setIsActive(true)} className={`p-2 border ${isActive&&"bg-[#E7FE29]"} ${isActive&&"text-black"} border-gray-300 border-r-0 rounded-l-xl cursor-pointer`}>Avaliable</button>
+          <button onClick={()=>setIsActive(false)} className={`p-2 border border-gray-300 border-l-0 rounded-r-xl cursor-pointer ${isActive||"bg-[#E7FE29]"} ${isActive||"text-black"}`}>Selected(0)</button>
+        </div>
       </div>
-     </div>
-    <Suspense fallback ={<div className='flex justify-center items-center'><span className="loading loading-spinner w-20 "></span></div>}>
-       <AvailablePlayers playersPromiss={playersPromiss} />
-    </Suspense>
-     {/* <SelectedPlayers/> */}
+      {
+        isActive ? <Suspense fallback={<div className='flex justify-center items-center'><span className="loading loading-spinner w-20 "></span></div>}>
+          <AvailablePlayers playersPromiss={playersPromiss} />
+        </Suspense>
+        : <SelectedPlayers />
+      }
     </>
   )
 }
